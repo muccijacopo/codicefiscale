8afa80adf593ff775305fb9a84f1b02fc7e25049
muccijacopo <mucci.jacopo@gmail.com>
2020-12-17 17:31:54 +0100

    footer styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,12 +5,11 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer>
-      <img className="react" src="./assets/images/react.png"></img>
-      <a href="https://web.dev/progressive-web-apps/">
-        <img className="pwa" src="./assets/images/pwa.svg"></img>
-      </a>
       <a href="https://github.com/muccijacopo/codicefiscale">
         <img className="github" src="./assets/images/github.svg"></img>
+      </a>
+      <a href="https://web.dev/progressive-web-apps/">
+        <img className="pwa" src="./assets/images/pwa.svg"></img>
       </a>
     </footer>
   );
